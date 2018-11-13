13c4f2a6128b54ba9909bdea0a6c0cee7eea94a2
sekti <sektis90@gmail.com>
2018-11-13 16:52:54 +0900

    gitignore 에서 /Modules 삭제 어느정도 개발 진행되고 모듈별로 개발진행할때 추가하기

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <!-- Styles -->
         <style>
             html, body {
@@ -94,5 +94,6 @@
                 </div>
             </div>
         </div>
+        <script src="{{ mix('js/app.js') }}"></script>
     </body>
 </html>
